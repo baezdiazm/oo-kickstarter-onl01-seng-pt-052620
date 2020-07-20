53d4b7e2a2622ab18d6de4ee0402c337fbcd83ab
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-07-20 02:54:17 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,13 +2,10 @@ class Backer
   attr_reader :name
 
   def initialize(name, title)
-    @name = name
+    self.name = name
     @baked_potatoes = []
     @title = title
   end
 
-  def name
-    @name
-  end
 
 end
